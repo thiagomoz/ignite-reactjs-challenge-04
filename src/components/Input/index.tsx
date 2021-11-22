dd42cbe594a082dfd08051b9f64221c8abef1f5d
Thiago Oliveira <thiagomo@me.com>
2021-11-22 14:01:49 -0300

    Finish Reactjs Challenge 04

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,60 @@
+import {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+
+import { IconType } from 'react-icons'
+
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface ICustomInputProps {
+  name: string;
+  icon?: IconType;
+}
+
+type IInputProps = JSX.IntrinsicElements['input'] & ICustomInputProps;
+
+export const Input = ({ name, icon: Icon, ...rest }: IInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
